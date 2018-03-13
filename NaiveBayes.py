73f7b82245efe75732de23b37a436192c8b13348
Dante <welchdan@mail.gvsu.edu>
2018-03-12 21:31:11 -0400

    start Bayes, thanks cam white

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,3 +1,29 @@
-class NaiveBayes:
-	def __init__(thing):
+class Document():
+	def __init__(self, text):
+		self.bag_of_words = {}
+		self.size = 0
+		text = text.split()
+
+		for word in text:
+			if word in self.bag_of_words:
+				self.bag_of_words[word] += 1
+			else:
+				self.bag_of_words[word] = 1
+
+			self.size += 1
+
+	def get_word_count(self, word):
+		res = 0
+		if(word in self.wordBag):
+			res = self.wordBag[word]
+
+		return res
+
+
+class NaiveBayes():
+	def __init__(self, thing):
 		self.thing = thing
+
+
+doc1 = Document("hate shit sue")
+doc2 = Document("love happy bob")
